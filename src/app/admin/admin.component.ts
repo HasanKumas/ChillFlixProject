e965fa33db2bf7268edbdd2e57200404b633b70c
Sam (Hisham) <33641929+hackoor83@users.noreply.github.com>
2020-05-14 23:41:41 +0200

    Newsam (#10)
    * working on movie adding form
    
    * Styling movie adding form redesigned
    
    * Login button menu added. Edit movie fixed. Other styling fixed.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../shared/services/movie.service';
+import { Movie } from '../shared/models/movie';
+import { ConfirmationModalComponent } from '../modals/confirmation-modal/confirmation-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  public movies: Movie[] = [];
 
-  constructor() { }
+  constructor(
+    private readonly movieService: MovieService,
+    private readonly ngbModalService: NgbModal
+  ) {}
 
-  ngOnInit(): void {
+  // adds a movie to backend
+  public addMovie(movie: Movie): void {
+    const modal = this.ngbModalService.open(ConfirmationModalComponent);
+    const modalComponent = modal.componentInstance as ConfirmationModalComponent;
+
+    modalComponent.text = `Are you sure you want to add movie
+    ${movie.title}?`;
+
+    modalComponent.title = 'Are you sure?';
+
+    modal.result.then(
+      () => {
+        this.movieService.addMovie(movie).subscribe((newMovie) => {
+          alert(`The movie has been added`);
+          this.movies.push(newMovie);
+        });
+      },
+      () => {
+        // Rejected the operation.
+      }
+    );
   }
 
+  ngOnInit(): void {
+    const movies$ = this.movieService.getMovies();
+    movies$.subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
 }
--- src/app/movie-adding-form/movie-adding-form.component.ts
+++ src/app/movie-adding-form/movie-adding-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MovieService } from '../shared/services/movie.service';
 import { Movie } from '../shared/models/movie';
@@ -6,6 +6,7 @@ import { CategoryService } from '../shared/services/category.service';
 import { Category } from '../shared/models/category';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationModalComponent } from '../modals/confirmation-modal/confirmation-modal.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-adding-form',
@@ -13,6 +14,21 @@ import { ConfirmationModalComponent } from '../modals/confirmation-modal/confirm
   styleUrls: ['./movie-adding-form.component.css'],
 })
 export class MovieAddingFormComponent implements OnInit {
+  public movieTitle: string;
+  public movieYear: number;
+  public movieId: number;
+  public movieCategory: Category;
+  public youtubeId: string;
+  public description: string;
+  public movieRating: number;
+
+  @ViewChild('movieInfoInputs') movieInfoInputs;
+  @ViewChild('movieTitleInput') movieTitleInput;
+  @ViewChild('movieYearInput') movieYearInput;
+  @ViewChild('movieCategoryInput') movieCategoryInput;
+  @ViewChild('movieUrlInput') movieUrlInput;
+  @ViewChild('movieDescriptionInput') movieDescriptionInput;
+
   editField: string;
   public isCollapsed = true;
   public movies: Movie[] = [];
@@ -23,8 +39,36 @@ export class MovieAddingFormComponent implements OnInit {
   constructor(
     private readonly movieService: MovieService,
     private readonly categoryService: CategoryService,
-    private readonly ngbModalService: NgbModal
+    private readonly ngbModalService: NgbModal,
+    private readonly activatedRoute: ActivatedRoute
   ) {}
+
+  public currentMovieInfo(): void {
+    console.log('inside the currentMovieInfo function');
+    this.movieInfoInputs.nativeElement.disabled = false;
+  }
+
+  /**
+   * Update Movie details
+   */
+  public updateMovie(movie: Movie): void {
+    const newMovie: Movie = {
+      id: movie.id,
+      title: this.movieTitleInput.nativeElement.value,
+      description: this.movieDescriptionInput.nativeElement.value,
+      year: this.movieYearInput.nativeElement.value,
+      url: this.movieUrlInput.nativeElement.value,
+      category: movie.category,
+    };
+    console.log(newMovie);
+
+    this.editMovie(newMovie);
+
+    // this.movieService.updateMovie(newMovie).subscribe((movie) => {
+    //   alert(`Movie updated ${movie.title}`);
+    // });
+    this.movieInfoInputs.nativeElement.disabled = true;
+  }
 
   // adds a movie to backend
   public addMovie(movie: Movie): void {
@@ -51,6 +95,9 @@ export class MovieAddingFormComponent implements OnInit {
 
   // initialize movies and categories
   ngOnInit(): void {
+    // To catch the id
+    this.movieId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+
     const categories$ = this.categoryService.getCategories();
     categories$.subscribe((categories) => {
       this.categories = categories;
@@ -94,6 +141,7 @@ export class MovieAddingFormComponent implements OnInit {
 
   // save changes to backend
   editMovie(movie: Movie) {
+    console.log('inside edit Movie function');
     const modal = this.ngbModalService.open(ConfirmationModalComponent);
     const modalComponent = modal.componentInstance as ConfirmationModalComponent;
 
